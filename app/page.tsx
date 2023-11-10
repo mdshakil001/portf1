455238d4453e05864a5709d01140c1f8e5b447ae
mdshakil001 <iamshakilab@gmail.com>
2023-11-11 00:04:31 +0600

    first

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main className="relative">
+      <div className="absolute top-4 left-4">site is under constructing <span>&#128543;</span></div>
+    </main>
+  )
+}
